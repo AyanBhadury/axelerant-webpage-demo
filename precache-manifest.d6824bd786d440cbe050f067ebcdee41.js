966005efa22d897a707b29d4a708d096ab302ce8
ayanbhaduryfc <ayon.bony@gmail.com>
2020-04-22 01:32:30 +0530

    Updates

---
+++ precache-manifest.d6824bd786d440cbe050f067ebcdee41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3ef26937ab982e6e0dcb02f6810d72d",
+    "url": "/axelerant-webpage-demo/index.html"
+  },
+  {
+    "revision": "532ee1150a3a9d68fdc8",
+    "url": "/axelerant-webpage-demo/static/css/main.c1050377.chunk.css"
+  },
+  {
+    "revision": "810852c4297ff7622530",
+    "url": "/axelerant-webpage-demo/static/js/2.e4d69735.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/axelerant-webpage-demo/static/js/2.e4d69735.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "532ee1150a3a9d68fdc8",
+    "url": "/axelerant-webpage-demo/static/js/main.a6a62fab.chunk.js"
+  },
+  {
+    "revision": "e41e45ac9d806c303013",
+    "url": "/axelerant-webpage-demo/static/js/runtime-main.f10fbd4e.js"
+  }
+]);
